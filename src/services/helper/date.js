6bf5b28d49d83687c5564b5f1f8f268992424cd6
John Cuba <john.cub@outlook.com>
2020-02-11 22:06:20 +0300

    add getStringDate func

---
+++ src/services/helper/date.js
@@ -0,0 +1,5 @@
+export const getDoubleNumb = numb => (numb < 9 ? `0${numb}` : numb);
+
+export const getStringDate = date => (`
+  ${date.getFullYear()}-${getDoubleNumb(1+date.getMonth())}-${date.getDate()}
+`);
