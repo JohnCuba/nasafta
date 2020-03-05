24747eb8b3c727a85cd95c04627d3a3cba3ce120
John Cuba <john.cub@outlook.com>
2020-03-05 15:53:24 +0300

    fix wrong default date format

--- src/services/helper/date.js
+++ src/services/helper/date.js
@@ -3,4 +3,4 @@ export const getDoubleNumb = numb => (numb < 9 ? `0${numb}` : numb);
 export const getStringDate = date =>
   `${date.getFullYear()}-${getDoubleNumb(
     1 + date.getMonth()
-  )}-${date.getDate()}`;
+  )}-${getDoubleNumb(date.getDate())}`;
