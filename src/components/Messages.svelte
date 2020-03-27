7ea2a7e22770d4946b7fd0f81107a63e43de9a08
Evgeniy Kubetskiy <032062@oaorsm.ru>
2020-03-27 18:50:45 +0300

    add message

--- src/components/Messages.svelte
+++ src/components/Messages.svelte
@@ -1,6 +1,7 @@
 <script>
   import { fly } from 'svelte/transition';
   import { stage, objects } from '../service/stores';
+  import utils from '../service/utils';
 
   let objectscount;
   objects.subscribe(value => {objectscount = value.length});
@@ -13,6 +14,14 @@
     on:introend='{() => stage.update(n => n+1)}'
   >
     В этот день рядом с землей пронеслись {objectscount} объектов!
+  </p>
+{:else if $stage === 2}
+  <p
+    in:fly='{{ y: 400, duration: 3000 }}'
+    out:fly='{{ y: -400, duration: 3000 }}'
+    on:introend='{() => stage.update(n => n+1)}'
+  >
+    И ближайший из них - {utils.getNearest($objects).name.replace(/[()]/g, '')}
   </p>
 {/if}
 
