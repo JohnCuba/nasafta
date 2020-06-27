7ed3326b82180bf1f7de0e7c5ef7ef8fb6615029
John Cuba <john.cub@outlook.com>
2020-06-27 10:02:05 +0300

    try to use map

--- src/components/3d/Earth.js
+++ src/components/3d/Earth.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
+const map = require('../../static/maps/earth.jpg');
 
 export default function getEarth() {
   let loader = new THREE.TextureLoader()
   let geometry = new THREE.SphereGeometry(2.5, 32, 32);
   let material = new THREE.MeshPhongMaterial()
+  console.log(map);
   loader.load(
-    'src/static/maps/earth.jpg',
+    map,
     (texture) => {
       material = new THREE.MeshPhongMaterial({map: texture})
     }
